d7953eaacab8129044e59736482f7b4bd10820f9
Vladimir Veljković <veljkovicvladimir86@gmail.com>
2021-10-14 16:47:56 +0200

    proba

--- src/main/java/com/kameni/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/kameni/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -9,30 +9,31 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UserDaoService {
-	
-	
+
+
 	private static List <User> users = new ArrayList<>();
-	
+
 	private static int usersCount  = 3;
 	static {
 		users.add(new User(1, "Vladimir", new Date()));
 		users.add(new User(2, "Draginja", new Date()));
 		users.add(new User(3, "Aleksandar", new Date()));
-		
+        users.add(new User(3, "Aleksandar", new Date()));
+
 	}
-	
+
 	public List<User> findAll(){
-		
+
 		return users;
 	}
-	
+
 	public User save(User user) {
 		if(user.getId()==null) {
 			user.setId(++usersCount);
 		}
 		users.add(user);
 		return user;
-		
+
 	}
 	public User findOne(int id) {
 		for (User user : users) {
@@ -42,7 +43,7 @@ public class UserDaoService {
 		}
 		return null;
 	}
-	
+
 	public User deleteById(int id) {
 		Iterator<User> iterator = users.iterator();
 		while (iterator.hasNext()) {
